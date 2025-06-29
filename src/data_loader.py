52bd0fcd8fab62bfaac775930d0c4359d84e49fa
David Adeleye <dave@D-Air.local>
2025-06-29 02:21:01 +0100

    Fixed a pylint issue

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,19 +1,21 @@
+"""
+This module contains functions for loading telecom user data from a URL.
+"""
+
+import pandas as pd
 
 def load_data_from_url(url):
     """Load data from a URL into a pandas DataFrame.
 
     Parameters
-    -----------
+    ----------
     url : str
         The URL of the data to load.
 
     Returns
     -------
-    df : pandas.DataFrame
+    pandas.DataFrame
         The loaded data.
     """
-    import pandas as pd
-
     df = pd.read_csv(url)
     return df
-    
